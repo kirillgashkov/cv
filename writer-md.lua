70700566589104d3ca2531dd3320ec5d32fa34ab
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-05-22 09:41:34 +0300

    Add writer bases

---
+++ writer-md.lua
@@ -0,0 +1,28 @@
+assert(tostring(PANDOC_API_VERSION) == "1.23.1", "Unsupported Pandoc API")
+
+local file = require("file")
+local log = require("log")
+
+local filter = {}
+
+---@param table_ Table
+filter.Table = function(table_)
+	return table_
+end
+
+---@param doc Pandoc
+---@param opts WriterOptions
+---@return string
+function Writer(doc, opts)
+	return pandoc.write(doc:walk(filter), "gfm", opts)
+end
+
+---@return string
+function Template()
+	local template = file.read_file(pandoc.path.join({ pandoc.path.directory(PANDOC_SCRIPT_FILE), "template.md" }))
+	assert(template, "Failed to read template file")
+	return template
+end
+
+---@type { [string]: boolean }
+Extensions = {}
