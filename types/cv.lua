3e7703a7312824d433b84dc44f4868119b9fa9ac
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-05-25 22:14:54 +0300

    Clean up CV types

--- types/cv.lua
+++ types/cv.lua
@@ -1,29 +1,43 @@
----@class Cv
+---@meta
+
+---@class config
+---@field date_formats { year: string, year_month: string, year_month_day: string }
+---@field date_months { [1]: string, [2]: string, [3]: string, [4]: string, [5]: string, [6]: string, [7]: string, [8]: string, [9]: string, [10]: string, [11]: string, [12]: string }
+---@field date_present string
+---@field education_header string
+---@field experience_at string
+---@field experience_header string
+---@field profile_header string
+---@field projects_header string
+---@field skills_header string
+---@field language string
+
+---@class cv
 ---@field updated_in string
 ---@field name string
 ---@field role string
 ---@field profile string
----@field contacts Contact[]
----@field skills Skill[]
----@field projects Project[]
----@field education Education[]
----@field experience Experience[]
+---@field contacts contact[]
+---@field skills skill[]
+---@field experience experience[]
+---@field projects project[]
+---@field education education[]
 
----@class Contact
+---@class contact
 ---@field name string
 ---@field description string
 
----@class Skill
+---@class skill
 ---@field name string
 ---@field description string
 
----@class Project
+---@class project
 ---@field name string
 ---@field description string
 ---@field started_in string
 ---@field finished_in? string
 
----@class Education
+---@class education
 ---@field name string
 ---@field description string
 ---@field organization string
@@ -32,7 +46,7 @@
 ---@field started_in string
 ---@field finished_in? string
 
----@class Experience
+---@class experience
 ---@field name string
 ---@field description string
 ---@field organization string
@@ -40,3 +54,5 @@
 ---@field location string
 ---@field started_in string
 ---@field finished_in? string
+
+---@alias item contact | skill | project | education | experience
