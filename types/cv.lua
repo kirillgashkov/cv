07766ab6a8ae28501630af9050fe4b46b68bf1f9
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-05-26 02:32:04 +0300

    Update config types

--- types/cv.lua
+++ types/cv.lua
@@ -1,16 +1,12 @@
 ---@meta
 
 ---@class config
----@field date_formats { year: string, year_month: string, year_month_day: string }
----@field date_months { [1]: string, [2]: string, [3]: string, [4]: string, [5]: string, [6]: string, [7]: string, [8]: string, [9]: string, [10]: string, [11]: string, [12]: string }
----@field date_present string
----@field education_header string
----@field experience_at string
----@field experience_header string
----@field profile_header string
----@field projects_header string
----@field skills_header string
 ---@field language string
+---@field profile_header string
+---@field skills_header string
+---@field experience_header string
+---@field projects_header string
+---@field education_header string
 
 ---@class cv
 ---@field updated_in string
